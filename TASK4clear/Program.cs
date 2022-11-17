cc9b4b0d02edc770de91b06a87ee437fffcedcca
Людмила <lv-glavbuh@yandex.ru>
2022-11-17 16:21:35 +0300

    Add task4

---
+++ TASK4clear/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// Console.Write("Введите число, которое хотите возвести в степень: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число равное степени: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// void Degree(int a, int b)
+// {
+//     int c = 1;
+//     for (int i = 0; i < b; i++) c *= a;
+//    // Console.WriteLine("Число {0} в степени {1} равно {2}", a, b, c);
+//    Console.WriteLine($"Число {a} в степени {b} равно {c}");
+// }
+// Degree(a,b);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+Console.Write("Введите длинну массива N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] EnterDislay()
+{
+int[] mass = new int[N];
+int i;
+Console.WriteLine("Введите числа:");
+for (i=0; i<mass.Length;i++) mass[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введенные числа:");
+for (i=0; i<mass.Length;i++) Console.Write("{0} ", mass[i]);
+return mass;
+}
+int [] mass=EnterDislay();
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello 12, World!");
+﻿// Console.WriteLine("Ввеите первое число");
+// int x1= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ввеите второе число");
+// int x2= Convert.ToInt32(Console.ReadLine());
+// // int sum=x1+x2;
+// // Console.WriteLine(sum);
+
+// void SumNumbers(int x1,int x2)
+// {
+
+//    int sum=x1+x2; 
+//    Console.WriteLine(sum);
+// }
+// SumNumbers(x1,x2);
+
+// int [] array = {8,6,3,2,4,7,8,9};
+// for (int i=0;i<array.Length;i++)
+// {
+//    Console.WriteLine(array[i]);
+// }
+
+int[] array = new int[10];
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+void SumArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = array[i] + sum;
+    }
+    Console.WriteLine(sum);
+}
+SumArray(array);
